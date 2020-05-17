4f7a105aa7efb90ea2526fff060b8900e2dde5ea
Audrum <t.me/audrum>
2020-05-16 20:12:13 -0500

    Activator for Office 365

---
+++ O365.cmd
@@ -0,0 +1,47 @@
+@echo off
+title Activador de Office 365 ProPlus
+cls
+echo ============================================================================
+echo Activador de Office 365
+echo https://t.me/audrum
+echo ============================================================================
+echo.
+(if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")
+(if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16")
+(for /f %%x in ('dir /b ..\root\Licenses16\proplusvl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)
+(for /f %%x in ('dir /b ..\root\Licenses16\proplusvl_mak*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)
+echo.
+echo ============================================================================
+echo Activando Office...
+cscript //nologo slmgr.vbs /ckms >nul
+cscript //nologo ospp.vbs /setprt:1688 >nul
+cscript //nologo ospp.vbs /unpkey:WFG99 >nul
+cscript //nologo ospp.vbs /unpkey:DRTFM >nul
+cscript //nologo ospp.vbs /unpkey:BTDRB >nul
+cscript //nologo ospp.vbs /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99 >nul
+set i=1
+:server
+if %i%==1 set KMS=kms7.MSGuides.com
+if %i%==2 set KMS=kms8.MSGuides.com
+if %i%==3 set KMS=kms9.MSGuides.com
+if %i%==4 goto notsupported
+cscript //nologo ospp.vbs /sethst:%KMS% >nul
+echo ============================================================================
+echo.
+echo.
+cscript //nologo ospp.vbs /act | find /i "successful" && 
+(echo.
+if errorlevel 2 exit) || 
+(echo La conexión con el servidor KMS falló! Intentando con otro servidor...
+echo Por favor esepre...
+echo.
+echo.
+set /a i+=1 
+goto server)
+goto halt
+:notsupported
+echo.
+echo ============================================================================
+echo Esta versión no es soportada.
+:halt
+pause >nul
