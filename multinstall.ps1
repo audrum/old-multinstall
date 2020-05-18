5a1068e068111a427bc63fd7e1b0539885c521fb
Audrum <t.me/audrum>
2020-05-18 16:02:57 -0500

    Change code and add imprivements

--- multinstall.ps1
+++ multinstall.ps1
@@ -6,201 +6,299 @@
     Write-Host "|   t.me/audrum    |" -ForegroundColor Green
     Write-Host "====================" -ForegroundColor Green
     Write-Host ""
-    Write-Host "La mayoría de funciones de este script solo funcionan en Windows 10" -ForegroundColor Magenta
+    Write-Host "Probado solo en Windows 10" -ForegroundColor Magenta
 }
 
-function Basic
+function ChocoEnvironment
 {
-    cls
     Write-Host "Verificando entorno..." -ForegroundColor Yellow
+    
+    $testchoco=choco -v
             
-    if (Test-Path $env:SYSTEMDRIVE\ProgramData\Chocolatey) 
+    if ($testchoco) 
     {
-        Write-Host "Entorno listo!" -ForegroundColor Green
+        Write-Host "Ya tiene instalado chocolatey versión $testchoco" -ForegroundColor Green
         }
             
      else 
      {
         Write-Host "Preparando entorno..." -ForegroundColor Magenta
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-        Write-Host "Entorno listo." -ForegroundColor Magenta
+        refreshenv
+        Write-Host "Entorno listo. Se instaló chocolatey versión $testchoco" -ForegroundColor Magenta
+        Start-Sleep -s 3
         }
-            
-      Write-Host "Instalando Google Chrome..." -ForegroundColor Yellow
-      choco install GoogleChrome -y
-      Write-Host "Instalación finalizada" -ForegroundColor Green
-      Write-Host ""
-      Write-Host "Instalando Firefox..." -ForegroundColor Yellow
-      choco install Firefox -y
-      Write-Host "Instalación finalizada" -ForegroundColor Green
-      Write-Host ""
-      Write-Host "Instalando 7-zip..." -ForegroundColor Yellow
-      choco install 7zip -y
-      Write-Host "Instalación finalizada" -ForegroundColor Green
-      Write-Host "Fin de las instalaciones" -ForegroundColor Green
-      Start-Sleep -s 5
 }
 
-function InstallOffice
+
+function InstallGoogleChrome
 {
-    cls
-    Write-Host "Verificando entorno..." -ForegroundColor Yellow
-            
-    if (Test-Path $env:SYSTEMDRIVE\ProgramData\Chocolatey) 
+    if (-not(Get-Item (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)').VersionInfo)
     {
-        Write-Host "Entorno listo!" -ForegroundColor Green
-        }
-            
-     else 
-     {
-        Write-Host "Preparando entorno..." -ForegroundColor Magenta
-        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-        Write-Host "Entorno listo." -ForegroundColor Magenta
-        }
+        $title    = 'Instalar Google Chrome'
+        $question = 'Google Chrome no está instalado ¿desea instalarlo?'
 
-    Write-Host "1. Descargar e instalar Office 365" -ForegroundColor Yellow
-    Write-Host "2. Solo instalar" -ForegroundColor Yellow
+        $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+        $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Si'))
+        $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
 
-    [int]$office = Read-Host "Seleccione una opción"
-
-    switch ($office)
-    {
-        '1'{
-            Write-Host "Iniciando descarga e instalación de Office 365..." -ForegroundColor Yellow
-            choco install office365proplus -y
-            Write-Host "Instalación  finalizada" -ForegroundColor Green
-            Start-Sleep -s 5
-            }
-
-         '2'{
-            Write-Host "Realice la instalación manualmente y regrese aquí para realizar la activación"
-            Write-Host "Regresando al menu en 5 segundos..." -ForegroundColor Yellow
-            Start-Sleep -s 5
-            cls
-            Menu
-            }
-    }
-}
-
-function ActivateWindows
-{
-    cls
-    Write-Host "Detectando la versión de Windows..." -ForegroundColor Yellow
-    $windows = (Get-WmiObject Win32_OperatingSystem).Caption
-    Write-Host "Actualmente tiene" $windows "instalado"
-    Write-Host "Activando..." -ForegroundColor Yellow
-
-    switch ($windows)
-    {
-        'Microsoft Windows 10 Home Single Language'
+        $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+        if ($decision -eq 0) 
         {
-            Write-Host "Instalando licencia..." -ForegroundColor Yellow
-            slmgr /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
-            Write-Host "Licencia instalada" -ForegroundColor Green
-            Write-Host "Conectando con el servidor de activación..." -ForegroundColor Yellow
-            slmgr /skms kms8.msguides.com
-            Write-Host "Conexion realizada" -ForegroundColor Green 
-            Write-Host "Activando" $windows -ForegroundColor Yellow
-            slmgr /ato
-            Write-Host "Proceso finalizado" -ForegroundColor Green
-            Start-Sleep -s 5
-            }
-
-         'Microsoft Windows 10 Home'
-         {
-            Write-Host "Instalando licencia..." -ForegroundColor Yellow
-            slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
-            Write-Host "Licencia instalada" -ForegroundColor Green
-            Write-Host "Conectando con el servidor de activación..." -ForegroundColor Yellow
-            slmgr /skms kms8.msguides.com
-            Write-Host "Conexion realizada" -ForegroundColor Green 
-            Write-Host "Activando" $windows -ForegroundColor Yellow
-            slmgr /ato
-            Write-Host "Proceso finalizado" -ForegroundColor Green
-            Start-Sleep -s 5
-            }
-
-         'Microsoft Windows 10 Pro'
-         {
-            Write-Host "Instalando licencia..." -ForegroundColor Yellow
-            slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
-            Write-Host "Licencia instalada" -ForegroundColor Green
-            Write-Host "Conectando con el servidor de activación..." -ForegroundColor Yellow
-            slmgr /skms kms8.msguides.com
-            Write-Host "Conexion realizada" -ForegroundColor Green 
-            Write-Host "Activando" $windows -ForegroundColor Yellow
-            slmgr /ato
-            Write-Host "Proceso finalizado" -ForegroundColor Green
-            Start-Sleep -s 5
-            }
+            Write-Host "Instalando Google Chrome..." -ForegroundColor Yellow
+            cinst GoogleChrome -y
+            Write-Host "Google Chrome se ha instalado" -ForegroundColor Green
+            Start-Sleep -s 3
+            Write-Host ""
+        } 
+        else 
+        {
+            Write-Host 'Ha decidido no instalar Google Chrome' -ForegroundColor Yellow
+            Write-Host ""
+            Start-Sleep -s 3
+        }
     }
-}
 
-function ActivateOffice
-{
-}
-
-function End
-{
-    Write-Host "1. Regresar al menu"
-    Write-Host "2. Salir"
-    $pick = Read-Host "Que desea hacer?"
-
-    switch ($pick)
+    else
     {
-        '1'{ 
-                Write-Host "Regresando al menu en 5 segundos..." -ForegroundColor Yellow
-                Start-Sleep -s 5
-                cls
-                Menu
-                }
-
-        '2' {
-                exit
-                }
+        Write-Host "Google Chrome ya está instalado, omitiendo instalación" -ForegroundColor Green
+        Write-Host ""
+        Start-Sleep -s 3
     }
+}
+
+function InstallFirefox
+{
+    if (-not(Test-Path "C:\Program Files\Mozilla Firefox\firefox.exe"))
+    {
+        $title    = 'Instalar Firefox'
+        $question = 'Firefox no está instalado ¿desea instalarlo?'
+
+        $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+        $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Si'))
+        $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+        $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+        if ($decision -eq 0) 
+        {
+            Write-Host "Instalando Firefox..." -ForegroundColor Yellow
+            cinst Firefox -y
+            Write-Host "Firefox se ha instalado" -ForegroundColor Green
+            Start-Sleep -s 3
+            Write-Host ""
+        } 
+        else 
+        {
+            Write-Host "Ha decidido no instalar Firefox" -ForegroundColor Yellow
+            Write-Host ""
+            Start-Sleep -s 3
+        }
+    }
+
+    else
+    {
+        Write-Host "Firefox ya está instalado, omitiendo instalación" -ForegroundColor Green
+        Write-Host ""
+        Start-Sleep -s 3
+    }
+
+}
+
+function Install7-zip
+{
+    if (-not(Test-Path "C:\Program Files\7-Zip\7zFM.exe"))
+    {
+        $title    = "Instalar 7-zip"
+        $question = "7-zip no está instalado ¿desea instalarlo?"
+
+        $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+        $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Si'))
+        $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+        $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+        if ($decision -eq 0) 
+        {
+            Write-Host "Instalando 7-zip..." -ForegroundColor Yellow
+            cinst 7zip -y
+            Write-Host "7-zip se ha instalado" -ForegroundColor Green
+            Start-Sleep -s 3
+            Write-Host ""
+        } 
+        else 
+        {
+            Write-Host "Ha decidido no instalar 7-zip" -ForegroundColor Yellow
+            Write-Host ""
+            Start-Sleep -s 3
+        }
+    }
+
+    else
+    {
+        Write-Host "7-zip ya está instalado, omitiendo instalación" -ForegroundColor Green
+        Write-Host ""
+        Start-Sleep -s 3
+    }
+}
+
+function InstallOffice365
+{
+    $title    = "Instalar Office 365"
+    $question = "¿Descargar e instalar Office 365?"
+
+    $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Si'))
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+    $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+    if ($decision -eq 0) 
+    {
+        Write-Host "Iniciando descarga e instalación de Office 365..." -ForegroundColor Yellow
+        cinst office365proplus -y
+        Write-Host "Instalación  finalizada" -ForegroundColor Green
+        Start-Sleep -s 3
+        Write-Host ""
+    } 
+        
+    else 
+    {
+        Write-Host "Ha decidido no instalar Office 365" -ForegroundColor Yellow
+        Write-Host ""
+        Start-Sleep -s 3
+    }
+}
+
+function ActivateWindows10
+{
+
+    $title    = "Activar Windows 10"
+    $question = "¿Desea ejecutar la activación de Windows 10?"
+
+    $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Si'))
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+    $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+    if ($decision -eq 0) 
+    {
+        Write-Host "Detectando la versión de Windows..." -ForegroundColor Yellow
+        $windows = (Get-WmiObject Win32_OperatingSystem).Caption
+        Write-Host "Actualmente tiene" $windows "instalado" -ForegroundColor Green
+        Start-Sleep -s 3
+        Write-Host "Activando..." -ForegroundColor Yellow
+        Start-Sleep -s 2
+
+        switch ($windows)
+        {
+            'Microsoft Windows 10 Home Single Language'
+            {
+                Write-Host "Instalando licencia..." -ForegroundColor Yellow
+                slmgr /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
+                Start-Sleep -s 2
+                Write-Host "Licencia instalada" -ForegroundColor Green
+                Write-Host "Conectando con el servidor de activación..." -ForegroundColor Yellow
+                slmgr /skms kms8.msguides.com
+                Start-Sleep -s 2
+                Write-Host "Conexion realizada" -ForegroundColor Green 
+                Write-Host "Activando" $windows -ForegroundColor Yellow
+                slmgr /ato
+                Start-Sleep -s 2
+                Write-Host "Windows 10 Home Single Language ahora está activado!" -ForegroundColor Green
+                Start-Sleep -s 5
+            }
+
+            'Microsoft Windows 10 Home'
+            {
+                Write-Host "Instalando licencia..." -ForegroundColor Yellow
+                slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+                Start-Sleep -s 2
+                Write-Host "Licencia instalada" -ForegroundColor Green
+                Write-Host "Conectando con el servidor de activación..." -ForegroundColor Yellow
+                slmgr /skms kms8.msguides.com
+                Start-Sleep -s 2
+                Write-Host "Conexion realizada" -ForegroundColor Green 
+                Write-Host "Activando" $windows -ForegroundColor Yellow
+                slmgr /ato
+                Start-Sleep -s 2
+                Write-Host "Windows 10 Home ahora está activado!" -ForegroundColor Green
+                Start-Sleep -s 5
+            }
+
+            'Microsoft Windows 10 Pro'
+            {
+                Write-Host "Instalando licencia..." -ForegroundColor Yellow
+                slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+                Start-Sleep -s 2
+                Write-Host "Licencia instalada" -ForegroundColor Green
+                Write-Host "Conectando con el servidor de activación..." -ForegroundColor Yellow
+                slmgr /skms kms8.msguides.com
+                Start-Sleep -s 2
+                Write-Host "Conexion realizada" -ForegroundColor Green 
+                Write-Host "Activando" $windows -ForegroundColor Yellow
+                slmgr /ato
+                Start-Sleep -s 2
+                Write-Host "Windows 10 Pro ahora está activado!" -ForegroundColor Green
+                Start-Sleep -s 5
+            }
+        }
+    } 
+        
+    else 
+    {
+        Write-Host "Ha decidido no activar Windows 10" -ForegroundColor Yellow
+        Write-Host ""
+        Start-Sleep -s 3
+    }
+
+    
+}
+
+function ActivateOffice365
+{
 }
 
 function menu
 {
+    cls
     Banner
     Write-Host ""
-    Write-Host "1. Instalar lo básico (Chrome, Firefox, 7-zip)" -ForegroundColor Yellow
-    Write-Host "2. Instalar Office" -ForegroundColor Yellow
-    Write-Host "3. Activar Windows 10" -ForegroundColor Yellow
-    Write-Host "4. Activar Office" -ForegroundColor Yellow
-    Write-Host "5. Ejecutar todas las acciones anteriores" -ForegroundColor Yellow
-    Write-Host "6. Salir" -ForegroundColor Yellow
+    Write-Host "1. Instalar Google Chrome" -ForegroundColor Yellow
+    Write-Host "2. Instalar Firefox" -ForegroundColor Yellow
+    Write-Host "3. Instalar 7-zip" -ForegroundColor Yellow
+    Write-Host "4. Instalar Office" -ForegroundColor Yellow
+    Write-Host "5. Activar Windows 10" -ForegroundColor Yellow
+    Write-Host "6. Activar Office" -ForegroundColor Yellow
+    Write-Host "7. Salir" -ForegroundColor Yellow
     Write-Host ""
 
-    [int]$option = Read-Host "Seleccione una opción"
+    [int]$option = Read-Host "Seleccione la opción que desea ejecutar"
     
     switch ($option)
     {
         '1' {
-                basic
+                InstallGoogleChrome
             }
 
         '2' {
-                InstallOffice
+                InstallFirefox
             }
 
         '3' {
-                ActivateWindows
+                Install7-zip
             }
 
         '4' {
-                ActivateOffice
+                InstallOffice365
             }
 
         '5' {
-                Basic
-                InstallOffice
-                ActivateWindows
-                ActivateOffice
+                ActivateWindows10
             }
 
         '6' {
+                ActivateOffice365
+            }
+
+        '7' {
                 exit
             }
 
@@ -208,6 +306,35 @@ function menu
 
 }
 
-menu  
+function Option
+{
+    $title    = "Finalizado"
+    $question = "Se ha completado el proceso, ¿quiere repetir alguna parte del proceso?"
 
+    $choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Si'))
+    $choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
 
+    $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+    if ($decision -eq 0) 
+    {
+        menu
+    } 
+        
+    else 
+    {
+        Write-Host "Saliendo..." -ForegroundColor Yellow
+        Write-Host ""
+        Start-Sleep -s 3
+        exit
+    }
+}
+
+Banner
+ChocoEnvironment
+InstallGoogleChrome
+InstallFirefox
+Install7-zip
+InstallOffice365
+ActivateWindows10
+Option
