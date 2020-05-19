c2a575e91b53c0b1371c985933b11d4a3b94a713
Audrum <t.me/audrum>
2020-05-19 14:16:23 -0500

    fix indentation

--- O2019.ps1
+++ O2019.ps1
@@ -21,46 +21,49 @@ function ActivateOffice
     Write-Host "Activando Office..." -ForegroundColor Green
     Start-Sleep -s 3
     if(Test-Path "$env:ProgramFiles\Microsoft Office\Office16")
-            {
-                Set-Location "$env:ProgramFiles\Microsoft Office\Office16"
+    {
+        Set-Location "$env:ProgramFiles\Microsoft Office\Office16"
 
-                Get-ChildItem "$env:ProgramFiles\Microsoft Office\root\Licenses16\" | Foreach-Object {
-                if($_.Name.StartsWith('ProPlus2019VL'))
-                {
-                    cscript ospp.vbs /inslic:"..\root\Licenses16\$_"
-                }
-                }
+        Get-ChildItem "$env:ProgramFiles\Microsoft Office\root\Licenses16\" | Foreach-Object {
+        if($_.Name.StartsWith('ProPlus2019VL'))
+        {
+            cscript ospp.vbs /inslic:"..\root\Licenses16\$_"
+        }
+        }
 
-                cscript ospp.vbs /setprt:1688
-                cscript ospp.vbs /unpkey:6MWKP >nul
-                cscript ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP
-                cscript ospp.vbs /sethst:kms8.msguides.com
-                cscript ospp.vbs /act
+        cscript ospp.vbs /setprt:1688
+        cscript ospp.vbs /unpkey:6MWKP >nul
+        cscript ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP
+        cscript ospp.vbs /sethst:kms8.msguides.com
+        cscript ospp.vbs /act
 
-                Write-Host "Office se ha activado satisfactoriamente" -ForegroundColor Green
-                Write-Host ""
-                Start-Sleep -s 3
-            }
+        Write-Host "Office se ha activado satisfactoriamente" -ForegroundColor Green
+        Write-Host ""
+        Start-Sleep -s 3
+    }
 
-            else
-            {
-                Set-Location "$env:ProgramFiles(x86)\Microsoft Office\Office16"
+    else
+    {
+        Set-Location "$env:ProgramFiles(x86)\Microsoft Office\Office16"
 
-                Get-ChildItem "$env:ProgramFiles(x86)\Microsoft Office\root\Licenses16" | Foreach-Object {
-                if($_.Name.StartsWith('ProPlus2019VL'))
-                {
-                    cscript ospp.vbs /inslic:"..\root\Licenses16\$_"
-                }
-                }
+        Get-ChildItem "$env:ProgramFiles(x86)\Microsoft Office\root\Licenses16" | Foreach-Object {
+        if($_.Name.StartsWith('ProPlus2019VL'))
+        {
+            cscript ospp.vbs /inslic:"..\root\Licenses16\$_"
+        }
+        }
 
-                cscript ospp.vbs /setprt:1688
-                cscript ospp.vbs /unpkey:6MWKP >nul
-                cscript ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP
-                cscript ospp.vbs /sethst:kms8.msguides.com
-                cscript ospp.vbs /act
+        cscript ospp.vbs /setprt:1688
+        cscript ospp.vbs /unpkey:6MWKP >nul
+        cscript ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP
+        cscript ospp.vbs /sethst:kms8.msguides.com
+        cscript ospp.vbs /act
 
-                Write-Host "Office se ha activado satisfactoriamente" -ForegroundColor Green
-                Write-Host ""
-                Start-Sleep -s 3
-            }
+        Write-Host "Office se ha activado satisfactoriamente" -ForegroundColor Green
+        Write-Host ""
+        Start-Sleep -s 3
+    }
 }
+
+Banner
+ActivateOffice
