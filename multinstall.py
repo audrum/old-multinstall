bb1e734dd480cf1e9ccfa449645fa3eb8196aa53
Audrum <t.me/audrum>
2021-10-14 09:46:47 -0500

    Fixed Office activation

--- multinstall.py
+++ multinstall.py
@@ -83,21 +83,51 @@ def activate_windows():
         os.system("slmgr /ato")
         print("===%s has been activated..." % value)
 
+    elif value == "Windows 11 Home Single Language":
+        os.system("slmgr /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH")
+        os.system("slmgr /skms kms8.msguides.com")
+        os.system("slmgr /ato")
+        print("===%s has been activated..." % value)
+    
+    elif value == "Windows 11 Home":
+        os.system("slmgr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99")
+        os.system("slmgr /skms kms8.msguides.com")
+        os.system("slmgr /ato")
+        print("===%s has been activated..." % value)
+
+    elif value == "Windows 11 Pro":
+        os.system("slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX")
+        os.system("slmgr /skms kms8.msguides.com")
+        os.system("slmgr /ato")
+        print("===%s has been activated..." % value)
+
     else:
         print("This program can't activate this Windows version, contact the creator for further help")
 
 def activate_office():
+    print("Activating..")
     file = open("actoffice.cmd", "a")
     file.write("@echo off\n")
     file.write('''
-    (if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")&(if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16")&(for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2019VL*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x")&(for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2019VL*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x)
+    @echo off
+    title Multinstall&cls&echo =====================================================================================&echo #Project: Activating Microsoft Office&echo =====================================================================================&echo.&echo #Supported products:&echo - Microsoft Office Standard 2019&echo - Microsoft Office Professional Plus 2019&echo.&echo.&(if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles%\Microsoft Office\Office16")&(if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" cd /d "%ProgramFiles(x86)%\Microsoft Office\Office16")&(for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2019VL*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)&(for /f %%x in ('dir /b ..\root\Licenses16\ProPlus2019VL*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul)&echo.&echo ============================================================================&echo Activating your Office...&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo ospp.vbs /setprt:1688 >nul&cscript //nologo ospp.vbs /unpkey:6MWKP >nul&set i=1&cscript //nologo ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP >nul||goto notsupported
+    :skms
+    if %i% GTR 10 goto busy
+    if %i% EQU 1 set KMS=kms7.MSGuides.com
+    if %i% EQU 2 set KMS=s8.now.im
+    if %i% EQU 3 set KMS=s9.now.im
+    if %i% GTR 3 goto ato
+    cscript //nologo ospp.vbs /sethst:%KMS% >nul
+    :ato
+    echo ============================================================================&echo.&echo.&cscript //nologo ospp.vbs /act | find /i "successful" && (echo.&echo ============================================================================&echo.&echo #Repository: github.com/audrum&echo.&echo #Please feel free to contact me at t.me/audrum if you have any questions or concerns.&echo.&echo ============================================================================) || (echo The connection to the KMS server failed! Trying to connect to another one... & echo Please wait... & echo. & echo. & set /a i+=1 & goto skms)
+    goto halt
+    :notsupported
+    echo ============================================================================&echo.&echo Sorry, your version is not supported.&echo.&goto halt
+    :busy
+    echo ============================================================================&echo.&echo Sorry, the server is busy and can't respond to your request. Please try again.&echo.
+    :halt
+    pause >nul
     ''')
-    file.write("\n")
-    file.write("cscript ospp.vbs /setprt:1688\n")
-    file.write("cscript ospp.vbs /unpkey:6MWKP >nul\n")
-    file.write("cscript ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP\n")
-    file.write("cscript ospp.vbs /sethst:kms8.msguides.com\n")
-    file.write("cscript ospp.vbs /act\n")
     file.close()
 
     activate = subprocess.Popen(["actoffice.cmd"], stdout=sys.stdout)
